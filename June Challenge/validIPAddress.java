118e344632b31ccec394df608c018f0090d287f7
KartikSareen18 <kartiksareen18@gmail.com>
2020-06-17 18:42:50 +0530

    DAY-16 Validate IP Address

--- June Challenge/validIPAddress.java
+++ June Challenge/validIPAddress.java
@@ -91,3 +91,19 @@ class Solution {
         
     }
 }
+
+// // USING REGEX 
+// class Solution {
+//     public String validIPAddress(String IP) {
+       
+//         String ipv4="(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+//         if(IP.matches(ipv4))
+//            return "IPv4";
+        
+//         String ipv6="((([0-9a-fA-F]{1,4})\\:){7}([0-9a-fA-F]{1,4}))";
+//         if(IP.matches(ipv6))
+//            return "IPv6";
+//        return "Neither";
+        
+//     }
+// }
